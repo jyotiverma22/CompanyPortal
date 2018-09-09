db066c65104473ad9a925c80adc8c5f6a6a4d865
jyotiverma22 <jyotverma22@gmail.com>
2018-09-09 20:06:41 +0530

    design

--- CompanyPortal/App_Start/BundleConfig.cs
+++ CompanyPortal/App_Start/BundleConfig.cs
@@ -21,7 +21,8 @@ namespace CompanyPortal
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/lib/site.css"));
 
 
             //js  
