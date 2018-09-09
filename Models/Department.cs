221e8fd1ca9f2f2975028b8cea388e1be8e49f62
jyotiverma22 <jyotverma22@gmail.com>
2018-09-10 00:36:44 +0530

    added models

---
+++ Models/Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+   public class Department
+    {
+        public int DId{ get; set; }
+
+        public string Dname{ get; set; }
+
+        public string Active{ get; set; }
+
+        public List<Registration> Users { get; set; }
+    }
+}
---
+++ Models/Role.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+   public class Role
+    {
+        public int RID { get; set; }
+        public string RoleName{ get; set; }
+        public string Active { get; set; }
+        public List<Registration> Users { get; set; }
+    }
+}
