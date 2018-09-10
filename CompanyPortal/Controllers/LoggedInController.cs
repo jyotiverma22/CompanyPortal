15be67d1e3c09760d0081516f0e7c0e4c937be12
jyotiverma22 <jyotverma22@gmail.com>
2018-09-09 21:01:57 -0700

    minor

--- CompanyPortal/Controllers/LoggedInController.cs
+++ CompanyPortal/Controllers/LoggedInController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -11,7 +12,7 @@ namespace CompanyPortal.Controllers
         // GET: LoggedIn
         public ActionResult Index()
         {
-            return View();
+                return View();
         }
     }
 }
