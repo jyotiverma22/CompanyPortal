29bce2fe5ff617299894af3d891956374b8b3efa
jyotiverma22 <jyotverma22@gmail.com>
2018-09-12 05:36:42 -0700

    home page

--- CompanyPortal/App_Start/BundleConfig.cs
+++ CompanyPortal/App_Start/BundleConfig.cs
@@ -19,6 +19,9 @@ namespace CompanyPortal
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                "~/lib/js/responsive.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/Site.css",
---
+++ CompanyPortal/lib/js/responsive.js
@@ -0,0 +1,20 @@
+﻿function helo() {
+    Console.log("hello");
+}
+var c = 0;
+function navbarHeight() {
+    if (c == 0) {
+
+        document.getElementById("header").style.height = '300px';
+        c = 1;
+    }
+    
+    else {
+        document.getElementById("header").style.height = '80px';
+
+        c = 0;
+        }
+
+   
+   
+}
